5d017121aa0cb0e60f08fa613a9649153bfa087e
Denis Evhuta <megadesby@gmail.com>
2015-10-02 14:49:44 +0300

    Signed-off-by: Denis Evhuta <megadesby@gmail.com>

---
+++ src/AdminavelServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Evhuta\Adminavel;
+
+use Illuminate\Support\ServiceProvider;
+
+class AdminavelServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        require __DIR__ . '/Http/routes.php';
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('adminavel', function($app) {
+            return new Adminavel;
+        });
+    }
+}
